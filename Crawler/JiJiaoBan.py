f5d0d7c862e6867bc3a2af5bc8b5acc661267ced
domg2019 <49177986+domg2019@users.noreply.github.com>
2024-09-12 18:21:34 +0800

    Update JiJiaoBan.py

--- Crawler/JiJiaoBan.py
+++ Crawler/JiJiaoBan.py
@@ -1,10 +1,11 @@
 import requests
 
-url="https://www.bb2y.com/page128"
+url = "https://www.bb2y.com/page128"
 
-dic = {
+head = {
    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0"
 }
 
+response = requests.get(url, headers=head)
 
-resp=requests.get(url, headers=dic)
+print(response.text)
